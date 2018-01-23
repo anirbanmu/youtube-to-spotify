738f18cc36b25ad65af24d0a309fdac4fef35a20
Anirban Mukhopadhyay <anirbanmu@outlook.com>
2018-01-22 19:00:35 -0600

    Generate new console application

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace youtube_to_spotify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
